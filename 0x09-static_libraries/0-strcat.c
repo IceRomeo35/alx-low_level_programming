888d17d406fb71b427f55447f33b594400717020
Romeo <iceromeo35@gmail.com>
2023-03-10 15:58:16 -0800

    libmya

--- 0x09-static_libraries/0-strcat.c
+++ 0x09-static_libraries/0-strcat.c
@@ -1,12 +1,12 @@
 #include "main.h"
- /**
- ** _strcat - concatenates two strings
+/**
+ * _strcat - concatenates two strings
  * @dest: input value
  * @src: input value
  *
  * Return: void
  */
-char *_strcat(char *dest, char *src);
+char *_strcat(char *dest, char *src)
 {
 	int i;
 	int j;
@@ -27,3 +27,9 @@ char *_strcat(char *dest, char *src);
 	dest[i] = '\0';
 	return (dest);
 }
+
+
+
+
+
+
