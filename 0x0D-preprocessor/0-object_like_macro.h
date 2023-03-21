f6a21aec2e30a16c47c85921ccd656b2395764c4
Romeo <iceromeo35@gmail.com>
2023-03-20 17:15:03 -0700

    Preprocessor

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,14 +1,7 @@
-#ifndef PI_H
-#define PI_H
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
 
-/*
-* File: 1-pi.h
-* Auth: Romeo Aduful
-* Desc: Header file that defines a macro named PI
-*       as an abbreviation for the token 3.14159265359.
-*/
-
-#define PI 3.14159265359
+#define SIZE 1024
 
 #endif
 
