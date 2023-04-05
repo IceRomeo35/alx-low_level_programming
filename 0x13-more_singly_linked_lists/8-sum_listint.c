c893739aeaa3434da050697c13e5caa681e080dc
IceRomeo35.IceRomeo35 <iceromeo35@gmail.com>
2023-04-04 19:45:06 -0700

    sumlist

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -6,14 +6,13 @@
  * Return: If the list is empty - 0.
  *         Otherwise - the sum of all the data.
  */
-
 int sum_listint(listint_t *head)
 {
 	int sum = 0;
 
 	while (head)
 	{
-		sum =+ head->n;
+		sum += head->n;
 		head = head->next;
 	}
 
